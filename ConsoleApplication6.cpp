234af78f4a5eb60a12b56d23727baa98f879d707
LENOVO <azizbishkekn2@gmail.com>
2023-01-25 17:00:03 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+struct info {
+	std::string city;
+	std::string street;
+	int number;
+	int index;
+};
+void end(std::string cit,std::string stree,int numbe,int inde) {
+	std::cout << "city: "<<cit<<"\n";
+	std::cout << "street: "<<stree<<"\n";
+	std::cout << "number: "<<numbe<<"\n";
+	std::cout << "index: "<<inde<<"\n";
+}
+int main(int argc, char** argv) {
+	info pers;
+	pers.city = "Bishkek";
+	pers.street = "Chuy pr.";
+	pers.number = 47;
+	pers.index = 735920572;
+	end(pers.city,pers.street,pers.number,pers.index);
+}
